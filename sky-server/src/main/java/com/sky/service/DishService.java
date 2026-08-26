515745d534d6a60b155ff16b84d245c748d06bc6
Shrak <13502200258@163.com>
2026-08-26 16:32:59 +0800

    实现菜品分页查询的功能

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,6 +1,8 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 import org.springframework.stereotype.Service;
 
 
@@ -8,4 +10,6 @@ public interface DishService {
 
 
     void saveWithFlavor(DishDTO dishDTO);
+
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
